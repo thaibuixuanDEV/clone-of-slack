07a1e12864fee8a04c9fc57e6172c44ce4e0a87b
ThaiBui <thaibuixuan34@gmail.com>
2020-04-05 17:23:49 +0700

    basic modling

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # association
+  has_many :message
+  has_many :chatroom_users
+  has_many :chatrooms, through: :chatroom_users
+end
