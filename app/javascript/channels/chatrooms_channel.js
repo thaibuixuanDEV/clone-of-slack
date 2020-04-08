839871f5320bc68a5101e5ea9ab8d1767933ce55
ThaiBui <thaibuixuan34@gmail.com>
2020-04-08 17:20:49 +0700

    bold chatroom when has new messages not yet read

--- app/javascript/channels/chatrooms_channel.js
+++ app/javascript/channels/chatrooms_channel.js
@@ -10,7 +10,11 @@ consumer.subscriptions.create("ChatroomsChannel", {
   },
 
   received(data) {
-    console.log(data);
-    $("[data-behavior='messages']").append(data.message);
+    var dataBehavior = $(`[data-behavior='messages'][data-chatroom_id='${data.chatroom_id}']`);
+    if(dataBehavior.length != 0){
+      dataBehavior.append(data.message);
+    } else {
+      $(`[data-behavior='chatroom-link'][data-chatroom-id='${data.chatroom_id}']`).css('font-weight', 'bold');
+    }
   }
 });
